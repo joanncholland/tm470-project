9161bd0b3274602111a2c6b38614546a0dfdbe91
Joann Holland <joann.c.holland@btinternet.com>
2021-08-18 15:30:26 +0100

    testing contact form submission

--- src/pages/Support/Support.js
+++ src/pages/Support/Support.js
@@ -6,7 +6,34 @@ export default function Support() {
   return (
     <div className="container">
       <h1>Support</h1>
-      <p>Support page goes here</p>
+      <form
+        name="contact"
+        method="POST"
+        netlify
+        netlify-honeypot="bot-field"
+        hidden
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+            Your Name:{" "}
+            <input required autoFocus={true} type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input required type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea required name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   );
 }
