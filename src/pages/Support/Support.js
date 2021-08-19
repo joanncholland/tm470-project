363a45488135c5080ac4901f0b33b8ae26b53332
Joann Holland <joann.c.holland@btinternet.com>
2021-08-19 15:52:53 +0100

    testing contact form submission

--- src/pages/Support/Support.js
+++ src/pages/Support/Support.js
@@ -1,37 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import "./Support.scss";
 
 export default function Support() {
-  //   const [name, setName] = useState(null);
-  //   const [email, setEmail] = useState(null);
-  //   const [message, setMessage] = useState(null);
+  const history = useHistory();
 
-  //   const encode = (data) => {
-  //     return Object.keys(data)
-  //       .map(
-  //         (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-  //       )
-  //       .join("&");
-  //   };
-
-  //   const handleSubmit = (e) => {
-  //     fetch("/", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //       body: encode({ "form-name": "contact", name, email, message }),
-  //     })
-  //       .then(() => console.log("form sent"))
-  //       .catch((error) => alert(error));
-
-  //     e.preventDefault();
-  //   };
-
+  const onSubmit = (e) => {
+    e.preventDefault();
+    history.pushState("/thanks");
+  };
   return (
     <div className="container">
       <h1>Support</h1>
       <form
-        onSubmit="submit"
+        onSubmit={onSubmit}
         name="contact"
         data-netlify="true"
         method="post"
@@ -40,34 +23,17 @@ export default function Support() {
         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
-            Your Name:{" "}
-            <input
-              type="text"
-              name="name"
-              //   value={name}
-              //   onChange={(e) => setName(e.target.value)}
-            />
+            Your Name: <input type="text" name="name" />
           </label>
         </p>
         <p>
           <label>
-            Your Email:{" "}
-            <input
-              type="email"
-              name="email"
-              //   value={email}
-              //   onChange={(e) => setEmail(e.target.value)}
-            />
+            Your Email: <input type="email" name="email" />
           </label>
         </p>
         <p>
           <label>
-            Message:{" "}
-            <textarea
-              name="message"
-              //   value={message}
-              //   onChange={(e) => setMessage(e.target.value)}
-            />
+            Message: <textarea name="message" />
           </label>
         </p>
         <p>
